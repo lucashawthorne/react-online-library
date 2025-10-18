2553b9faffa6288da735c6336ddaf15c61ad0f00
Raindomize <lucashpro17@gmail.com>
2025-10-17 23:03:39 -0700

    fix and improve discounted books section

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -10,25 +10,29 @@ const Book = ({ book }) => {
             <img src={book.url} alt="" className="book__img" />
           </figure>
         </a>
-        <div className="book__title">
-          <a href="/" className="book__title--link">
-            {book.title}
-          </a>
-        </div>
-        <div className="book__ratings">
-          {
-            new Array(4).fill(0).map((_, index) => <FontAwesomeIcon icon="star" key={index} />)
-          }
-        </div>
-        <div className="book__price">
-          {book.salePrice ? (
-            <>
-              <span className="book__price--normal">{book.originalPrice.toFixed(2)}</span>
-              {book.salePrice.toFixed(2)}
-            </>
-          ) : (
-            <>${ book.originalPrice.toFixed(2) }</>
-          )}
+        <div className="book__details">
+          <div className="book__title">
+            <a href="/" className="book__title--link">
+              {book.title}
+            </a>
+          </div>
+          <div className="book__ratings">
+            {new Array(4).fill(0).map((_, index) => (
+              <FontAwesomeIcon icon="star" key={index} />
+            ))}
+          </div>
+          <div className="book__price">
+            {book.salePrice ? (
+              <>
+                <span className="book__price--normal">
+                  ${book.originalPrice.toFixed(2)}
+                </span>
+                ${book.salePrice.toFixed(2)}
+              </>
+            ) : (
+              <>${book.originalPrice.toFixed(2)}</>
+            )}
+          </div>
         </div>
       </div>
     </div>
