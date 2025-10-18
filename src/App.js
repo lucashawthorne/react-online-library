bdd77aec27f29813e2da503b2f1e48de4f6ebdd4
Raindomize <lucashpro17@gmail.com>
2025-10-18 08:54:19 -0700

    finish home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Discounted from "./components/Discounted";
+import Explore from "./components/Explore";
 import Featured from "./components/Featured";
+import Footer from "./components/Footer";
 import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
@@ -12,6 +14,8 @@ function App() {
       <Highlights />
       <Featured />
       <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -17,9 +17,13 @@ const Book = ({ book }) => {
             </a>
           </div>
           <div className="book__ratings">
-            {new Array(4).fill(0).map((_, index) => (
+            {
+            new Array(Math.floor(book.rating)).fill(0).map((_, index) => (
               <FontAwesomeIcon icon="star" key={index} />
             ))}
+            {
+              !Number.isInteger(book.rating) && <FontAwesomeIcon icon="star-half-alt" />
+            }
           </div>
           <div className="book__price">
             {book.salePrice ? (
