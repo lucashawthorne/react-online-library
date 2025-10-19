7ed8184bd9f7da335d461dce4bbe0602f008733f
Raindomize <lucashpro17@gmail.com>
2025-10-18 22:12:29 -0700

    add sort functionality,  cleaned up nav/footer code, added burger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from '../assets/Library.svg'
 
 const Nav = () => {
+  function openMenu() {
+    document.body.classList += " menu--open";
+  }
+
+  function closeMenu() {
+    document.body.classList.remove("menu--open")
+  }
+
   return (
   <nav>
     <div className="nav__container">
@@ -11,27 +19,27 @@ const Nav = () => {
       </a>
       <ul className="nav__links">
         <li className="nav__list">
-          <a href="" className="nav__link">
+          <a href="/" className="nav__link">
             Home
           </a>
         </li>
         <li className="nav__list">
-          <a href="" className="nav__link">
+          <a href="/books" className="nav__link">
             Books
           </a>
         </li>
-        <button className="btn__menu">
+        <button className="btn__menu" onClick={openMenu}>
           <FontAwesomeIcon icon="bars" />
         </button>
         <li className="nav__icon">
-          <a href="/cast" className="nav__link">
+          <a href="/cart" className="nav__link">
             <FontAwesomeIcon icon ="shopping-cart" />
           </a>
           <span className="cart__length">2</span>
         </li>
       </ul>
       <div className="menu__backdrop">
-        <button className="btn__menu btn__menu--close">
+        <button className="btn__menu btn__menu--close" onClick={closeMenu}>
           <FontAwesomeIcon icon="times" />
         </button>
         <ul className="menu__links">
