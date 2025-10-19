fb1b4c18bb156c47a00ab0a8d957649609f3bf1b
Raindomize <lucashpro17@gmail.com>
2025-10-19 08:42:46 -0700

    add bookinfo page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/books" element={<Books books={books} />} />
+          <Route path="/books" exact element={<Books books={books} />} />
           <Route path="/books/1" element={<BookInfo books={books} />} />
         </Routes>
         <Footer />
