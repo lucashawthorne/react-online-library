705d5e727c0e70cfdcbbec4be73d58dc15b099f7
Raindomize <lucashpro17@gmail.com>
2025-10-19 21:18:28 -0700

    finalize cart functionality

--- src/App.js
+++ src/App.js
@@ -12,38 +12,76 @@ function App() {
   const [cart, setCart] = useState([]);
 
   function addToCart(book) {
-    const dupeItem = cart.find((item) => +item.id === +book.id)
+    const dupeItem = cart.find((item) => +item.id === +book.id);
     if (dupeItem) {
-      setCart(cart.map(item => {
-        if (item.id === dupeItem.id) {
-          return {
-            ...item,
-            quantity: item.quantity + 1,
+      setCart(
+        cart.map((item) => {
+          if (item.id === dupeItem.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          } else {
+            return item;
           }
-        }
-        else {
-          return item
-        }
-      }))
-    }
-    else {
-      setCart([...cart, {...book, quantity: 1 }])
+        })
+      );
+    } else {
+      setCart([...cart, { ...book, quantity: 1 }]);
     }
   }
 
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter;
+  }
+
   useEffect(() => {
-    console.log(cart)
-  }, [cart])
+    console.log(cart);
+  }, [cart]);
 
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()}/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" exact element={<Books books={books} />} />
-          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart}/>} />
-          <Route path="/cart" element={<Cart books={books} />} />
+          <Route
+            path="/books/:id"
+            element={<BookInfo books={books} addToCart={addToCart} />}
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                books={books}
+                cart={cart}
+                changeQuantity={changeQuantity}
+                removeItem={removeItem}
+              />
+            }
+          />
         </Routes>
         <Footer />
       </div>
